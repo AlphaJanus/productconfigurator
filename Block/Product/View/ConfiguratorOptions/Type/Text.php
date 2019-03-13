87a8293c3dc94ff00572a108c355c79fac5cf152
Andrew Stepanchuk <styopchik@gmail.com>
2019-03-13 15:25:01 +0200

    5264 Тип опциона Text улучшение. refs #5264 @4h30min

--- Block/Product/View/ConfiguratorOptions/Type/Text.php
+++ Block/Product/View/ConfiguratorOptions/Type/Text.php
@@ -20,4 +20,54 @@ class Text extends AbstractOptions
             ->getData('configurator_options/' . $this->getOption()->getId());
         return $configuredValue ? $configuredValue : $this->getOption()->getData('default_value');
     }
+
+    public function getValidationRules()
+    {
+        $textValidate = null;
+        $option = $this->getOption();
+        if ($option->getIsRequired()) {
+            $textValidate['required'] = true;
+        }
+        if ($min = $option->getData('min_value')) {
+            $textValidate['gte'] = (float)$min;
+        }
+        if ($max = $option->getData('max_value')) {
+            $textValidate['lte'] = (float)$max;
+        }
+        if ($option->getMaxCharacters()) {
+            $textValidate['maxlength'] = $option->getMaxCharacters();
+        }
+        $rulesArray = explode(' ', $option->getData('validation'));
+        $rules = [];
+        foreach ($rulesArray as $class) {
+            $rules = $this->mapRules($class, $rules);
+        }
+        $textValidate = array_merge($textValidate, $rules);
+        return $this->json->serialize($textValidate);
+    }
+
+    /**
+     * Map classes w. rules
+     *
+     * @param string $class
+     * @param array $rules
+     * @return array
+     * @since 101.0.0
+     */
+    protected function mapRules($class, array $rules)
+    {
+        switch ($class) {
+            case 'validate-number':
+            case 'validate-digits':
+            case 'validate-email':
+            case 'validate-url':
+            case 'validate-alpha':
+            case 'validate-alphanum':
+            case 'digits-two-after-comma':
+                $rules = array_merge($rules, [$class => true]);
+                break;
+        }
+
+        return $rules;
+    }
 }
